07a73b048acf1e84bb8fbcea336e1150c39a4b7f
Nathan Edwards <nathan@onedarnleyroad.com>
2017-07-19 14:32:53 +0100

    put some command stuff in

--- index.js
+++ index.js
@@ -1,31 +1,67 @@
-var Crawler = require("crawler");
-var url = require('url');
-var stringify = require('csv').stringify;
-var fs = require('fs');
-var colors = require('colors');
+const Crawler = require("crawler");
+const url = require('url');
+const stringify = require('csv').stringify;
+const fs = require('fs');
+const colors = require('colors');
+
+
+const y = require('yargs')
+    .describe('f', 'Load a config file'.blue)
+    .describe('u', 'Load a url'.blue)
+    .describe('o', 'Output to this file (overrides setting in input file)'.blue)
+    .help('h')
+
+const argv = y.argv;
+
+
+var _url = argv.u;
+var _file = argv.f;
+
+
+if (!_url || !_file) {
+    console.log("You must provide an input file or url".red);
+    y.showHelp();
+    process.exit();
+}
 
 
 
-var parent = process.argv[2];
+
+if (_file) {
+    const config = require( "./" + _file );
+    const parent = config.url;
+} else {
+    // _url is implied
+    const parent = _url;
+}
+
+if (!parent) {
+    console.log("Error - the URL to crawl is not valid: " + "Url Provided: ".red + parent );
+    process.exit();
+}
+
+const parentUrlObj = url.parse( parent );
+
 
 console.log( "Crawling ".grey + parent + "...".green);
 
+if (argv.o) {
+    const logfile = arvg.o;
+} else if (config.logFile) {
+    const logfile = config.logfile;
+} else {
+    const logfile = parentUrlObj.hostname + ".txt";
+}
 
 var data = [];
-var parentUrlObj = url.parse( parent );
-
-
-
-var logfile = parentUrlObj.hostname + ".txt";
 
 fs.writeFileSync(logfile, ''); // setup new file
 
-
+// Store urls Crawled
 var urlsCrawled = [];
 
 // setup CSV writer
 stringifier = stringify({delimiter: ','});
-
 
 var c = new Crawler({
     skipDuplicates: true,
